34e967f402b953793a6811b08640c002b04777ae
José Ferrari <licoverpa321@gmail.com>
2025-01-26 23:08:31 -0300

    ui improvements

--- src/app/calcular/page.tsx
+++ src/app/calcular/page.tsx
@@ -1,12 +1,28 @@
 import { Calculator } from "@/components/Calculator";
+import Link from "next/link";
 
 export default function CalcularPage() {
   return (
-    <div>
-      <div style={{ justifyItems: "center" }}>
-        <h1>Comece a calcular</h1>
+    <div
+      style={{
+        backgroundColor: "white",
+        margin: 50,
+        borderRadius: 10,
+      }}
+    >
+      <div style={{ alignItems: "initial", paddingTop: 20, paddingLeft: 20 }}>
+        <Link key={"home"} href={"/"}>
+          <p>Home</p>
+        </Link>
       </div>
-      <div>
+      <div
+        style={{ display: "flex", justifyContent: "space-evenly", padding: 20 }}
+      >
+        <div>
+          <h1>Comece a calcular</h1>
+        </div>
+      </div>
+      <div style={{ width: "100%", padding: 20 }}>
         <Calculator />
       </div>
     </div>
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -21,40 +21,55 @@ export const Calculator = () => {
 
   return (
     <>
+      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <div>
+          <div>
+            <h1>Lista de dispositivos</h1>
+          </div>
+          {devicesArray.map((e) => (
+            <div
+              key={e.id}
+              style={{ textAlign: "center", margin: 10, cursor: "pointer" }}
+            >
+              <p onClick={() => handleClick(e)}>{e.name}</p>
+            </div>
+          ))}
+        </div>
+        <div>
+          <div>
+            <h1>Lista de dispositivos selecionados</h1>
+          </div>
+          {selectedDevices.map((e, index) => (
+            <div key={index} style={{ textAlign: "center", margin: 10 }}>
+              <p onClick={() => handleClick(e)}>{e.name}</p>
+            </div>
+          ))}
+          <div style={{ textAlign: "center", margin: 10 }}>
+            <Button
+              color="#e74c3c"
+              text="Limpar dispositivos selecionados"
+              onClick={() => setSelectedDevices([])}
+            />
+          </div>
+        </div>
+      </div>
       <div
         style={{
           display: "flex",
-          justifyContent: "space-around",
-          marginTop: 50,
-          marginBottom: 50,
+          justifyContent: "space-evenly",
+          margin: 10,
+          padding: 10,
         }}
       >
-        <div>
-          <h1>Lista de dispositivos</h1>
-          {devicesArray.map((e) => (
-            <p key={e.id} onClick={() => handleClick(e)}>
-              {e.name}
-            </p>
-          ))}
+        <div style={{ textAlign: "center" }}>
+          <h1>Total kWh</h1>
+          {totalConsumokWh}
         </div>
-        <div>
-          <h1>Lista de dispositivos selecionados</h1>
-          {selectedDevices.map((e, index) => (
-            <p key={index}>{e.name}</p>
-          ))}
-          <Button
-            color="#e74c3c"
-            text="Limpar dispositivos selecionados"
-            onClick={() => setSelectedDevices([])}
-          />
+        <div style={{ textAlign: "center" }}>
+          <h1>Total gasto por hora</h1>
+          R$
+          {(totalConsumokWh * 0.824 + selectedState.tariff).toFixed(1)}
         </div>
-      </div>
-      <div style={{ justifyItems: "center" }}>
-        <h1>Total kWh</h1>
-        {totalConsumokWh}
-        <h1>Total gasto por hora</h1>
-        R$
-        {(totalConsumokWh * 0.824 + selectedState.tariff).toFixed(1)}
       </div>
     </>
   );
